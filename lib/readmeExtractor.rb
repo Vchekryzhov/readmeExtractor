10105e8cacdf0f62212d76dc90f370a8cbfc6ec6
Viktor C <vchekryzhov@ya.ru>
2024-04-02 00:18:48 +0200

    master: Add gem folder to gitignore

---
+++ lib/readmeExtractor.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "readmeExtractor/version"
+
+module ReadmeExtractor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/readmeExtractor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ReadmeExtractor do
+  it "has a version number" do
+    expect(ReadmeExtractor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
